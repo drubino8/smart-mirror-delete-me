a46f98977cd8258f4785adddefacbad004c5ba27
Dawson Rubino <155998903+drubino8@users.noreply.github.com>
2024-12-11 05:42:29 +0000

    Fixed weather not appearing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+
+
+interface ImportMetaEnv {
+
+    readonly VITE_SOME_KEY: string;
+  
+    readonly PUBLIC_WEATHER_API: string;
+  
+    // more env variables...
+  
+  }
+  
